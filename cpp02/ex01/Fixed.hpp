02bd9dd5d1f68773208c81c7a5ebba3e6dc661bc
el mehdi belfkih <mehdibm.az@gmail.com>
2024-02-07 16:12:03 +0100

    cpp01>ex06 : start

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -5,14 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/27 03:26:46 by ebelfkih          #+#    #+#             */
-/*   Updated: 2024/02/07 11:43:36 by ebelfkih         ###   ########.fr       */
+/*   Created: 2024/02/07 11:38:46 by ebelfkih          #+#    #+#             */
+/*   Updated: 2024/02/07 11:48:33 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
@@ -22,6 +23,10 @@ private:
 
 public:
     Fixed();
+    Fixed(const int val); // to do
+    Fixed(const float val); // to do
+    float toFloat( void ) const; // to do
+    int toInt( void ) const; // to do
     Fixed(const Fixed& obj);
     ~Fixed();
     Fixed& operator=(const Fixed& obj);
