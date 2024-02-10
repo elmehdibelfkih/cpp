efa9a7b096deb81f3189269fbe6977e34eecff90
el mehdi belfkih <mehdibm.az@gmail.com>
2024-02-10 19:17:04 +0100

    cpp03> : start

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -6,7 +6,30 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 17:52:06 by ebelfkih          #+#    #+#             */
-/*   Updated: 2024/02/10 17:52:07 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2024/02/10 19:04:33 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+#include <iostream>
+
+ScavTrap::ScavTrap(std::string name)
+{
+    std::cout << "ScavTrap constructor called" << std::endl;
+    this->name = name;
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap constructor called" << std::endl;
+}
+
+void ScavTrap::takeDamage(unsigned int amount)
+{
+    if (this->EnergyPoints > 0 && this->HitPoints > 0)
+    {
+        this->EnergyPoints--;
+        std::cout << this->name << " attack " << target << std::endl;
+    }
+}
 
--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -6,7 +6,17 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 17:52:18 by ebelfkih          #+#    #+#             */
-/*   Updated: 2024/02/10 17:52:19 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2024/02/10 19:00:52 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+    ScavTrap(std::string name);
+    ~ScavTrap();
+    void takeDamage(unsigned int amount);
+};
